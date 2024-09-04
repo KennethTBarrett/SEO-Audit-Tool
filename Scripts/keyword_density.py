78d690827a460f89de09be1d0b57465c41fe48c0
Kenneth Barrett <51110156+KennethTBarrett@users.noreply.github.com>
2024-09-04 16:45:53 -0400

    PEP8 Improvements

--- Scripts/keyword_density.py
+++ Scripts/keyword_density.py
@@ -9,8 +9,9 @@ nltk.download('wordnet')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger_eng')
 
+
 def get_wordnet_pos(word):
-    '''Convert NTLK POS tags to WordNet Compatible'''
+    """Convert NTLK POS tags to WordNet Compatible"""
     tag = nltk.pos_tag([word])[0][1][0].upper()
     tag_dict = {"J": wordnet.ADJ,
                 "N": wordnet.NOUN,
@@ -18,27 +19,30 @@ def get_wordnet_pos(word):
                 "R": wordnet.ADV}
     return tag_dict.get(tag, wordnet.NOUN)
 
+
 # def get_synonyms(word):
-#     '''Gets synonyms for keyword to ensure coverage'''
+#     """Gets synonyms for keyword to ensure coverage"""
 #     synonyms = set()
 #     for syn in wordnet.synsets(word):
 #         for lemma in syn.lemmas():
 #             synonyms.add(lemma.name())
 #     return synonyms
 
+
 # def lemmatize_text(text):
-#     '''Lemmatizes text before sending for keyword density calculation'''
+#     """Lemmatizes text before sending for keyword density calculation"""
 #     lemmatizer = WordNetLemmatizer()
 #     words = nltk.word_tokenize(text)
 #     lemmatized = [lemmatizer.lemmatize(word, get_wordnet_pos(word)) for word in words]
 #     return ' '.join(lemmatized)
 
 def calc_keyword_density(text, keywords):
-    '''Calculates keyword density'''
+    """Calculates keyword density"""
     vectorizer = CountVectorizer(vocabulary=keywords)
     word_count = vectorizer.fit_transform([text]).toarray().sum(axis=0)
     total_words = len(re.findall(r'\w+', text))
-    keyword_density = {word: (count / total_words) * 100 for word, count in zip(keywords, word_count)}
+    keyword_density = {word: (count / total_words) * 100 for word,
+                       count in zip(keywords, word_count)}
     return keyword_density
 
 
